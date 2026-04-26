39f5e2a2366e8979b88f039edab76118dbc79b44
sanjit <sanjit.est14@gmail.com>
2026-04-26 20:22:53 +0600

    Premitive Data types

---
+++ OldClasses/TypeScriptTechnocrat/BasicsTypesOfTypescript/module1/src/1.4.ts
@@ -0,0 +1,36 @@
+//* Basic Data Types
+
+// String
+
+let firstName: string = "Sanjit";
+
+// Number
+
+let roll: number = 124;
+
+// Boolean
+
+let isAdmin: boolean = true;
+
+// undefined
+
+let x: undefined = undefined;
+
+// null
+
+let y: null = null;
+
+// any
+
+let d;
+
+// Array
+
+let frineds: string[] = ["Max", "Jack"];
+let eligibleRollList: number[] = [1, 2, 3, 4];
+
+// tuple --> Array --> Order --> Types of Values
+
+let coordinate: [number, number] = [123, 345];
+
+let ageName: [number, string, boolean] = [34, "Mr. X", false];
