8e16a8132df1fa1231692534b5073484bf1ae209
sanjit <sanjit.est14@gmail.com>
2026-04-30 23:54:19 +0600

    Explore Non Primitive Types

---
+++ Mission1/BeATypeScriptTechnocrat/Module1/ExploreBasicTypesOfTypescript/src/nonPrimitive.ts
@@ -0,0 +1,9 @@
+//* Non Primitive Data --> Array, Object
+
+//* TS --> tuple
+
+let bazarList: string[] = ["eggs", "milk", "suger"];
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 2, "sugar", 3];
+
+let couple: [string, string] = ["Husband", "wife"];
