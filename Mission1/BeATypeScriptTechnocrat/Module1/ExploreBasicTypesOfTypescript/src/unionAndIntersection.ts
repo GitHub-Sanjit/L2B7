a295565506f0559a16162204c8d8f49f9bf46851
sanjit <sanjit.est14@gmail.com>
2026-05-01 18:58:45 +0600

    Union and Intersection

---
+++ Mission1/BeATypeScriptTechnocrat/Module1/ExploreBasicTypesOfTypescript/src/unionAndIntersection.ts
@@ -0,0 +1,13 @@
+//* Union
+
+type UserRole = "admin" | "user" | "guest";
+
+const getDashboard = (role: UserRole) => {
+  if (role === "admin") {
+    return "Admin Dashboard";
+  } else if (role === "user") {
+    return "User Dashboard";
+  }else{
+    return 'Guest Dashboard'
+  }
+};
