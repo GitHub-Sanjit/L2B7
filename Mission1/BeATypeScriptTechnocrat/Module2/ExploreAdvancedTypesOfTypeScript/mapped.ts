b2c429d4cb17a708cb8e495c9978b30084dfc691
sanjit <sanjit.est14@gmail.com>
2026-05-03 18:34:50 +0600

    mapped types

---
+++ Mission1/BeATypeScriptTechnocrat/Module2/ExploreAdvancedTypesOfTypeScript/mapped.ts
@@ -0,0 +1,34 @@
+//* mapped types
+
+const arrayOfNum: number[] = [1, 4, 6];
+
+const arrayOfString: string[] = ["1", "4", "6"];
+
+const arrayOfStringUsingMap: string[] = arrayOfNum.map((num) => num.toString());
+
+console.log(arrayOfStringUsingMap);
+
+type AreaOfNum = {
+  height: number;
+  width: number;
+};
+
+type Height = AreaOfNum["height"];
+
+// type AreaOfString = {
+//   height: string;
+//   width: string;
+// };
+
+// type AreaOfString = {
+//   [key in keyof AreaOfNum]: string;
+// };
+
+type Area<T> = {
+  [key in keyof T]: T[key];
+};
+
+const area1: Area<{ height: string; width: number }> = {
+  height: "50",
+  width: 20,
+};
