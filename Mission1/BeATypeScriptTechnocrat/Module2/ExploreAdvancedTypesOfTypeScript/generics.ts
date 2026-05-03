8c613c149c55b567d8e22c30c5b4f097f9abb2b8
sanjit <sanjit.est14@gmail.com>
2026-05-03 11:36:35 +0600

    Generics in TypeScript

---
+++ Mission1/BeATypeScriptTechnocrat/Module2/ExploreAdvancedTypesOfTypeScript/generics.ts
@@ -0,0 +1,33 @@
+//* dynamically generalize : Generic
+
+// const friends: string[] = ["A", "B", "C"];
+
+type GenericArray<T> = Array<T>;
+
+const friends: GenericArray<string> = ["A", "B", "C"];
+
+// const rollNumbers: number[] = [4, 7, 12];
+const rollNumbers: GenericArray<number> = [4, 7, 12];
+
+// const isEligibleList: boolean[] = [true, false, true];
+const isEligibleList: GenericArray<boolean> = [true, false, true];
+
+const userList: GenericArray<{ name: string; age: number }> = [
+  {
+    name: "X",
+    age: 24,
+  },
+  {
+    name: "Y",
+    age: 25,
+  },
+  {
+    name: "Z",
+    age: 45,
+  },
+];
+
+type Coordinates<X, Y> = [X, Y];
+
+const coordinates1: Coordinates<number, number> = [20, 30];
+const coordinates2: Coordinates<string, string> = ["20", "30"];
