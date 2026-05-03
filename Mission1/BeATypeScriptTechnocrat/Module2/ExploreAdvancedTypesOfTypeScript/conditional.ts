15d64f2e0024463c8d27b2caab319470409bad37
sanjit <sanjit.est14@gmail.com>
2026-05-03 17:53:21 +0600

    Conditional Type

---
+++ Mission1/BeATypeScriptTechnocrat/Module2/ExploreAdvancedTypesOfTypeScript/conditional.ts
@@ -0,0 +1,17 @@
+//* conditional type
+
+type A = null;
+
+type B = undefined;
+
+type C = A extends null ? true : B extends undefined ? true : false;
+
+type RichPeoplesVehicles = {
+  bike: string;
+  car: string;
+  ship: string;
+};
+
+type CheckVehicle<T> = T extends keyof RichPeoplesVehicles ? true : false;
+
+type HasBike = CheckVehicle<"bike">;
