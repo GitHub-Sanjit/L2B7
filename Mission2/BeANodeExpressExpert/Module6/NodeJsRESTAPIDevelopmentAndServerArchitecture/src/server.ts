71319a3f4c69683c5f9b75d5370897eb6dc56a55
sanjit <sanjit.est14@gmail.com>
2026-05-10 23:17:12 +0600

    Route Handling with http request

--- Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/server.ts
+++ Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/server.ts
@@ -1,8 +1,24 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-  console.log(req);
-});
+const server: Server = createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    // console.log(req.url);
+    // console.log(req.method);
+    const url = req.url;
+    const method = req.method;
+    if (url === "/" && method === "GET") {
+      // console.log(`This is Root Route`);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "This is Root route" }));
+    } else if (url?.startsWith("/products")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "This is Products route" }));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "Page Not Found" }));
+    }
+  },
+);
 
 server.listen(5000, () => {
   console.log(`Server is running on the PORT 5000`);
