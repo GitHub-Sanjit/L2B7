e8c509cacb750e37d46ef29280dda48a8e9dc616
sanjit <sanjit.est14@gmail.com>
2026-05-11 19:34:21 +0600

    Get Method Operations

--- Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
+++ Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { Iproduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -8,10 +9,24 @@ export const productController = (
   const url = req.url;
   const method = req.method;
 
-  const products = readProduct();
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(
-    JSON.stringify({ message: "This is Products route", data: products }),
-  );
+  if (url === "/products" && method === "GET") {
+    const products = readProduct();
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "This is Products route", data: products }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: Iproduct) => p.id === id);
+    // console.log(product)
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({ message: "This is Products route", data: product }),
+    );
+  }
 };
