744a0b9bf3989f2cd993b4eeda26e8a9405d8a7c
sanjit <sanjit.est14@gmail.com>
2026-05-11 18:54:29 +0600

    fs module, Reading file

--- Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
+++ Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,38 +8,7 @@ export const productController = (
   const url = req.url;
   const method = req.method;
 
-  const products = [
-    {
-      id: 1,
-      name: "Laptop",
-      price: 850,
-      quantity: 5,
-    },
-    {
-      id: 2,
-      name: "Mouse",
-      price: 25,
-      quantity: 20,
-    },
-    {
-      id: 3,
-      name: "Keyboard",
-      price: 45,
-      quantity: 15,
-    },
-    {
-      id: 4,
-      name: "Monitor",
-      price: 200,
-      quantity: 8,
-    },
-    {
-      id: 5,
-      name: "Headphone",
-      price: 60,
-      quantity: 12,
-    },
-  ];
+  const products = readProduct();
 
   res.writeHead(200, { "content-type": "application/json" });
   res.end(
