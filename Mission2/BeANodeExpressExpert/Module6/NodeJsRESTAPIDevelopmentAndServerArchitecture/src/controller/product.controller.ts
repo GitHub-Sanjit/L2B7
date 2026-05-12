c68ad4ba0c0c9a973a6a59e435471ed96dbee2c7
sanjit <sanjit.est14@gmail.com>
2026-05-12 08:24:53 +0600

    Executing Delete Method

--- Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
+++ Mission2/BeANodeExpressExpert/Module6/NodeJsRESTAPIDevelopmentAndServerArchitecture/src/controller/product.controller.ts
@@ -25,6 +25,15 @@ export const productController = async (
     const products = readProduct();
     const product = products.find((p: Iproduct) => p.id === id);
     // console.log(product)
+    if (!product) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product Not Found",
+          data: product,
+        }),
+      );
+    }
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
@@ -74,6 +83,29 @@ export const productController = async (
       JSON.stringify({
         message: "Product Updated Successfully",
         data: products[index],
+      }),
+    );
+  } else if (method === "DELETE" && id !== null) {
+    const products = readProduct();
+    const index = products.findIndex((p: Iproduct) => p.id === id);
+    const deletedProduct = products[index];
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Product Not Found",
+          data: null,
+        }),
+      );
+    }
+    products.splice(index, 1);
+    console.log(products);
+    insertProduct(products);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product deleted Succesfully",
+        data: deletedProduct,
       }),
     );
   }
