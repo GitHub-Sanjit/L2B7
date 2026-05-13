e0b3e50a316b2631fdf41752929384e947f8d7fd
sanjit <sanjit.est14@gmail.com>
2026-05-13 23:18:45 +0600

    Delete user with delete method

--- Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
+++ Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
@@ -131,7 +131,7 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       `
-        UPDATE users SET name=$1,password=$2,age=$3,is_active=$4 WHERE id=$5
+        UPDATE users SET name=COALESCE($1 ,name),password=COALESCE($2,password),age=COALESCE($3, age),is_active=COALESCE($4 ,is_active) WHERE id=$5
         RETURNING *
     `,
       [name, password, age, is_active, id],
@@ -149,6 +149,38 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
       success: true,
       message: "User updated successfully",
       data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+            DELETE FROM users WHERE id=$1
+            `,
+      [id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: {},
     });
   } catch (error: any) {
     res.status(500).json({
