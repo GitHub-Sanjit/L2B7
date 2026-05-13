160c0591fcd794807acc8b34bafcaeed873a6a5c
sanjit <sanjit.est14@gmail.com>
2026-05-13 19:22:11 +0600

    Express Request and Response

--- Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
+++ Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
@@ -1,9 +1,21 @@
-import express from "express";
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 5000;
 
-app.get("/user", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.get("/user", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ message: "Hi there, How are you doing?", author: "Next Level" });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
