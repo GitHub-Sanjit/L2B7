0a237115354e1a568ed85235cb169084525b737a
sanjit <sanjit.est14@gmail.com>
2026-05-13 20:06:54 +0600

    Executing Pool and creating table with postgres

--- Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
+++ Mission2/BeANodeExpressExpert/Module7/ExpressJs_ServerArchitecture_And_Database_Integration/src/server.ts
@@ -16,6 +16,28 @@ const pool = new Pool({
     "",
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            age INT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log({ error: error });
+  }
+};
+
+initDB()
+
 app.get("/user", (req: Request, res: Response) => {
   res
     .status(200)
