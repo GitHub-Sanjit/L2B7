8f96255b645d12190558a922b21398c2af53be3b
sanjit <sanjit.est14@gmail.com>
2026-05-15 21:15:24 +0600

    Refactoring User module

--- Mission2/BeANodeExpressExpert/Module8/AdvancedBackendStructuringAndUserAuthentication/src/modules/user/user.controller.ts
+++ Mission2/BeANodeExpressExpert/Module8/AdvancedBackendStructuringAndUserAuthentication/src/modules/user/user.controller.ts
@@ -26,6 +26,115 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.getAllUsersFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: (await result).rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+const getSingleUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await userServices.getSingleUserFromDB(id as string);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, password, age, is_active } = req.body;
+
+  try {
+    const result = await userServices.updateUserFromDB(id as string, {
+      name,
+      password,
+      age,
+      is_active,
+    });
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await userServices.deleteUserFromDB(id as string);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not Found",
+        data: {},
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
 export const userController = {
   createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
--- Mission2/BeANodeExpressExpert/Module8/AdvancedBackendStructuringAndUserAuthentication/src/modules/user/user.service.ts
+++ Mission2/BeANodeExpressExpert/Module8/AdvancedBackendStructuringAndUserAuthentication/src/modules/user/user.service.ts
@@ -13,6 +13,70 @@ const createUserIntoDB = async (payload: IUser) => {
   return result;
 };
 
+const getAllUsersFromDB = async () => {
+  const result = pool.query(`
+            SELECT * from users
+        `);
+  return result;
+};
+
+const getSingleUserFromDB = async (id: string) => {
+  const result = await pool.query(
+    `
+        SELECT * FROM users WHERE id=$1
+        `,
+    [id],
+  );
+  return result;
+};
+
+const updateUserFromDB = async (id: string, payload: any) => {
+  const { name, password, age, is_active } = payload;
+  const result = await pool.query(
+    `
+        UPDATE users SET name=COALESCE($1 ,name),password=COALESCE($2,password),age=COALESCE($3, age),is_active=COALESCE($4 ,is_active) WHERE id=$5
+        RETURNING *
+    `,
+    [name, password, age, is_active, id],
+  );
+  return result;
+};
+
+const deleteUserFromDB1 = async (id: string) => {
+  const result = await pool.query(
+    `
+            SELECT * FROM users WHERE id=$1
+            `,
+    [id],
+  );
+  const result2 = await pool.query(
+    `
+            DELETE FROM users WHERE id=$1
+            `,
+    [id],
+  );
+  console.log("deleteUserFromDB", result);
+  return result;
+};
+
+const deleteUserFromDB = async (id: string) => {
+  const result = await pool.query(
+    `
+    DELETE FROM users
+    WHERE id = $1
+    RETURNING *
+    `,
+    [id],
+  );
+
+  console.log("Deleted user:", result.rows[0]);
+  return result;
+};
+
 export const userServices = {
   createUserIntoDB,
+  getAllUsersFromDB,
+  getSingleUserFromDB,
+  updateUserFromDB,
+  deleteUserFromDB,
 };
