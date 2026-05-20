38885c48fa0f8d8192ae99d73841898cc0a92db0
sanjit <sanjit.est14@gmail.com>
2026-05-20 10:54:14 +0600

    Book table added

--- OldClasses/assignment_nodejs/src/db/index.ts
+++ OldClasses/assignment_nodejs/src/db/index.ts
@@ -8,12 +8,19 @@ export const pool = new Pool({
 export const initDB = async () => {
   try {
     await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
+  CREATE TABLE IF NOT EXISTS books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    isbn VARCHAR(20) UNIQUE NOT NULL,
+    description TEXT,
+    copies INT NOT NULL CHECK (copies >= 0),
+    available BOOLEAN GENERATED ALWAYS AS (copies > 0) STORED,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  );
+`);
 
     await pool.query(`
       CREATE TABLE IF NOT EXISTS profiles (
