3b831ec11f62b81ac35132201dbebe242a90e55d
sanjit <sanjit.est14@gmail.com>
2026-06-26 23:41:48 +0600

    Module 21.1

--- Mission4/PrismaORM/Module22/src/modules/auth/auth.controller.ts
+++ Mission4/PrismaORM/Module22/src/modules/auth/auth.controller.ts
@@ -4,33 +4,58 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { authService } from "./auth.service";
 
-const loginUser = catchAsync(async (req : Request, res : Response, next : NextFunction) => {
+const loginUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const {accessToken, refreshToken} = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
 
     res.cookie("accessToken", accessToken, {
-        httpOnly : true,
-        secure : false,
-        sameSite : "none",
-        maxAge : 1000 * 60 * 60 * 24 // 24 hour or 1 day
-    })
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    });
 
     res.cookie("refreshToken", refreshToken, {
-        httpOnly : true,
-        secure : false,
-        sameSite : "none",
-        maxAge : 1000 * 60 * 60 * 24 * 7 // 7 day
-    })
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 day
+    });
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "User logged in successfully",
-        data: { accessToken, refreshToken }
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User logged in successfully",
+      data: { accessToken, refreshToken },
     });
-});
+  },
+);
+
+const refreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { refreshToken } = req.cookies;
+
+    const newAccessToken = await authService.refreshToken(refreshToken);
+
+    res.cookie("accessToken", newAccessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Access token refreshed successfully",
+      data: { accessToken: newAccessToken },
+    });
+  },
+);
 
 export const authController = {
-    loginUser
-}
+  loginUser,
+  refreshToken,
+};
