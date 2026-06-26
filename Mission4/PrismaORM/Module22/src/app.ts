006f241aee09a9202108709609124f11d893dec4
sanjit <sanjit.est14@gmail.com>
2026-06-26 22:10:22 +0600

    Module 22 starter

---
+++ Mission4/PrismaORM/Module22/src/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { userRoutes } from "./modules/user/user.route";
+
+
+
+const app : Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
+
+// app.post()
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+export default app;
