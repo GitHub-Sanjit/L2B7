21929ac50b5abfffbc935096da85e969aabd10db
sanjit <sanjit.est14@gmail.com>
2026-06-27 17:34:14 +0600

    Post and commment

--- Mission4/PrismaORM/Module22/src/app.ts
+++ Mission4/PrismaORM/Module22/src/app.ts
@@ -27,5 +27,7 @@ app.get("/",(req : Request, res : Response) => {
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
 
 export default app;
